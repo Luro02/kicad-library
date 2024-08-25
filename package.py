26ec02d51430448fd68dcfc6bea4bcfe8bfb7c2b
Luro02 <24826124+Luro02@users.noreply.github.com>
2024-08-25 12:28:45 +0200

    generate repo on release

--- package.py
+++ package.py
@@ -1,28 +1,28 @@
 # Based on https://github.com/espressif/kicad-libraries/blob/main/package.py
 # Some samples are in here:https://gitlab.com/kicad/addons/metadata
 import json
-from os import path, walk as os_walk
 import zipfile
-import os
 import re
 import hashlib
 import sys
 
-directories_to_zip = [
-    "3dmodels",
-    "footprints",
-    "symbols",
-    "resources"
-]
+from datetime import datetime, timezone
+from pathlib import Path
 
+DIRECTORIES_TO_ZIP = [
+    Path("3dmodels"),
+    Path("footprints"),
+    Path("symbols"),
+    Path("resources"),
+]
 
 KICAD_VERSION = "8.0.0"
 ZIP_FILE_NAME = "package.zip"
 DOWNLOAD_URL = "https://github.com/Luro02/kicad-library/releases/download/{VERSION}/{ZIP_FILE_NAME}"
+MAIN_URL = "https://raw.githubusercontent.com/Luro02/kicad-library/main/{FILE_NAME}"
 
 def create_json_string(zip_internal_metadata_json: dict):
     return json.dumps(zip_internal_metadata_json, indent=4)
-
 
 def read_template_json_file():
     with open('metadata.template.json', mode='r') as metadata_file:
@@ -30,7 +30,6 @@ def read_template_json_file():
         metadata_file.close()
 
         return metadat_template
-
 
 def create_zip_internal_metadata_json(version: str):
     template = read_template_json_file()
@@ -43,7 +42,6 @@ def create_zip_internal_metadata_json(version: str):
     template["versions"] = [version_item]
 
     return template
-
 
 def create_full_metadata_file(version: str, existing_versions: list, zip_size: int, zip_internal_size: int, zip_file_sha256: str):
     template = read_template_json_file()
@@ -66,23 +64,7 @@ def create_full_metadata_file(version: str, existing_versions: list, zip_size: i
     with open('metadata.json', mode='w') as metadata_file:
         metadata_file.write(create_json_string(template))
 
-
-def zip_directory(target: str, zip_handle: zipfile.ZipFile):
-    for root, dirs, files in os_walk(target):
-        for file in files:
-            current_file_path = path.join(root, file)
-            zip_handle.write(current_file_path, path.relpath(current_file_path, path.join(target, '..')))
-
-
-def package_directories_to_zip(zip_handle: zipfile.ZipFile):
-    for directory_to_zip in directories_to_zip:
-        zip_directory(directory_to_zip, zip_handle)
-
-
-def add_zip_internal_metadata_json(zip_handle: zipfile.ZipFile, version: str):
-    zip_internal_metadata_json = create_zip_internal_metadata_json(version)
-    zip_handle.writestr('metadata.json', create_json_string(zip_internal_metadata_json))
-
+    return template
 
 def calculate_zip_content_size(zip_handle: zipfile.ZipFile) -> int:
     zip_content_size = 0
@@ -92,31 +74,29 @@ def calculate_zip_content_size(zip_handle: zipfile.ZipFile) -> int:
 
     return zip_content_size
 
-
 # Return internal size
-def generate_release_zip_file(zip_file_path: str, version: str) -> int:
+def generate_release_zip_file(zip_file_path: Path, version: str) -> int:
     with zipfile.ZipFile(zip_file_path, 'w', zipfile.ZIP_DEFLATED) as zip_handle:
-        package_directories_to_zip(zip_handle)
-        add_zip_internal_metadata_json(zip_handle, version)
+        # add all directories to the zip file:
+        for directory_to_zip in DIRECTORIES_TO_ZIP:
+            # go through all files in the directory to zip
+            for path in directory_to_zip.rglob("*"):
+                # and add them to the zip file
+                zip_handle.write(path, path)
+
+        zip_handle.writestr('metadata.json', create_json_string(create_zip_internal_metadata_json(version)))
 
         return calculate_zip_content_size(zip_handle)
 
+def hash_data_sha256(data) -> str:
+    return hashlib.sha256(data).hexdigest()
 
-def generate_sha256(zip_file_path: str):
-    hash_gen = hashlib.sha256()
-
-    with open(zip_file_path, mode='rb') as zip_file:
-        while True:
-            data = zip_file.read(65536)
-            if not data:
-                break
-            hash_gen.update(data)
-
-    return hash_gen.hexdigest()
-
+def hash_file_sha256(filepath: Path) -> str:
+    with open(filepath, "rb") as fd:
+        return hashlib.file_digest(fd, 'sha256', _bufsize=65536).hexdigest()
 
 def read_all_existing_versions():
-    if os.path.isfile('metadata.json'):
+    if Path('metadata.json').is_file():
         with open('metadata.json') as f:
             metadata_file = json.load(f)
             f.close()
@@ -124,21 +104,55 @@ def read_all_existing_versions():
                 return metadata_file['versions']
     return []
 
-
-def check_version_already_exist(version, existing_versions):
-    for version_data in existing_versions:
-        if version_data["version"] == version:
-            return True
-
-    return False
-
-
 def get_version_from_user():
     version = input("Enter the new addon version (required format: major[.minor[.patch]]): ")
     return version.strip()
 
+def generate_repository_file(metadata, version):
+    with open("repository.template.json", "rb") as fd:
+        repo_template = json.load(fd)
 
-def package():
+    # create "packages" and "resources" sections?
+    with open("packages.json", "w") as fd:
+        fd.write(create_json_string({ "packages": [dict(metadata)] }))
+
+    utc_time = datetime.now(timezone.utc)
+    update_time_utc = utc_time.strftime('%Y-%m-%d %H:%M:%S')
+    update_timestamp = int(utc_time.timestamp())
+
+    repo_template["packages"] = {
+        "sha256": hash_file_sha256(Path("packages.json")),
+        "update_time_utc": update_time_utc,
+        "update_timestamp": update_timestamp,
+        "url": MAIN_URL.replace("{FILE_NAME}", "packages.json"),
+    }
+
+    # create the resources.zip file which contains the icon:
+    
+    # the resources zip file looks like this:
+    # /
+    # - com.github.user.plugin-name
+    #   - icon.png
+
+    # -> metadata["identifier"] for folder name
+    # -> resources/icon.png for the icon
+
+    zip_resources_path = Path('build') / "resources.zip"
+    resources_path = Path('resources')
+    with zipfile.ZipFile(zip_resources_path, 'w', zipfile.ZIP_DEFLATED) as zip_handle:
+        zip_handle.write(resources_path / "icon.png", f'{metadata["identifier"]}/icon.png')
+
+    repo_template["resources"] = {
+        "sha256": hash_file_sha256(zip_resources_path),
+        "update_time_utc": update_time_utc,
+        "update_timestamp": update_timestamp,
+        "url": DOWNLOAD_URL.replace("{VERSION}", version).replace("{ZIP_FILE_NAME}", "resources.zip"),
+    }
+
+    with open("repository.json", "w") as fd:
+        fd.write(create_json_string(repo_template))
+
+if __name__ == "__main__":
     print('This script helps to generate a new kicad addon release. \n')
     print('It generates the release zip file and the metadata.json \n\n')
 
@@ -152,28 +166,25 @@ def package():
             "Version string is invalid. Required format: major[.minor[.patch]] (major, minor, patch are numbers)")
 
     existing_versions = read_all_existing_versions()
-    if check_version_already_exist(version, existing_versions):
+    if version in [version_data["version"] for version_data in existing_versions]:
         raise ValueError("The specified version " + version + " already exists")
 
     print("Start packaging documents to zip\n")
-    if not os.path.isdir('build/'):
-        os.mkdir('build')
+    build_dir = Path("build/")
+    build_dir.mkdir(exist_ok=True)
 
-    zip_file_path = os.path.join('build', ZIP_FILE_NAME)
-
-    if os.path.isfile(zip_file_path):
-        os.remove(zip_file_path)
+    zip_file_path = build_dir / ZIP_FILE_NAME
+    # remove old files:
+    zip_file_path.unlink(missing_ok=True)
 
     zip_internal_size = generate_release_zip_file(zip_file_path, version)
-    zip_sha256 = generate_sha256(zip_file_path)
-    zip_size = os.stat(zip_file_path).st_size
+    zip_sha256 = hash_file_sha256(zip_file_path)
+    zip_size = zip_file_path.stat().st_size
 
-    print('Generated addon zip at path: ' + zip_file_path + '\n')
-
+    print(f'Generated addon zip at path: {zip_file_path}\n')
     print('Starting to generate metadata.json\n')
-    create_full_metadata_file(version, existing_versions, zip_size, zip_internal_size, zip_sha256)
+    metadata = create_full_metadata_file(version, existing_versions, zip_size, zip_internal_size, zip_sha256)
 
-    print('All necessary files are generated. Now follow the steps on release-kicad-addons.md')
+    generate_repository_file(metadata, version)
 
-if __name__ == "__main__":
-    package()
+    print('All necessary files have been generated.')
